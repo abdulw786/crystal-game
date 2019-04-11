d37b5b0390d1c865c7e2f60f5d27c58ddad3b267
abdul wahid hashoory <whashoory@gmail.com>
2019-04-11 01:54:14 -0500

    Ï

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,15 @@
 //Selects a random number 
 $(document).ready(function () {
     //The random number shown at the start of the game should be between 19 - 120.
-    var Random = Math.floor(Math.random() * 101 +19)
-    $("#RandomNumber").text(Random);
+
+    // var Random = Math.floor(Math.random() * 101 + 19)
     //  Decaring variables for tallies
     var wins = 0;
     var losses = 0;
     var totalScore = 0;
-    //append to the html id
+    var random = Math.floor(Math.random() * 101 + 19);
+    //get refrence
+    $("#RandomNumber").text(random);
     $("#totalwins").text(wins);
     $("#totallosses").text(losses);
 
@@ -17,39 +19,33 @@ $(document).ready(function () {
         wins++
         $("#totalwins").text(wins);
         resetGame();
-
     }
-    // increases the loss column whenever you loose  
     function lose() {
         alert("you lost");
         losses++;
         $("#totallosses").text(losses);
         resetGame();
+    }
+    var resetGame = function () {
+        random = Math.floor(Math.random() * 101 + 19);
+        totalScore = 0;
+        $("#RandomNumber").text(random);
+        $("#Guess").empty(totalScore);
 
-
-    }var resetGame = function(){
-        if (Totalscore =>randomNumber){
-        $("#Guess").empty();
-}}
+        // }
+    }
     //setting up the onclick function
-    $("#crystals").on("click", function () {
+    $(".crystals").on("click", function () {
         // Each crystal should have a random hidden value between 1 - 12.
         randomNumber = Math.floor(Math.random() * 12);
-        console.log(randomNumber);
         totalScore = totalScore + randomNumber;
-        $("#Guess").text(totalScore);
-        if (totalScore === Random) {
+         $("#Guess").text(totalScore);
+        if (totalScore === random) {
             winner();
-
-
+            resetGame();
+        } else if (totalScore > random) {
+            lose();
+            resetGame();
         }
-        else if (totalScore > Random) {
-            lose();            
-
-
-        }
-    });
-    resetGame();    
-
-
+    }); 
 });
